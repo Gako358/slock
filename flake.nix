c1a58dcbe0365a18fc5cdd7fb35f600a9748caf8
gako358 <gako358@outlook.com>
2022-07-29 15:22:26 +0200

    flake for slock install

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  description = "MerrinX slock build";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }: 
+      let
+        system = "x86_64-linux";
+	overlay = final: prev: {
+	  slock = prev.st.overrideAttrs (oldAttrs: rec {
+	    version = "main";
+	    src = builtins.path { path = ./.; name = "st"; };
+	    buildInputs = oldAttrs.buildInputs ++ [
+	    ];
+	  });
+	};
+	in
+	rec {
+	  overlays.default = overlay;
+	  checks.${system}.build = (
+            import nixpkgs {
+	      inherit system;
+	      overlays = [ overlay ];
+	    }
+	  ).slock;
+	};
+}
