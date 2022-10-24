71d8830757d4648a0cc127b2a9d09a88f0dc63c9
Gako358 <gako358@outlook.com>
2022-10-24 21:21:43 +0200

    corrected compiler error due too missing pkgs config input

--- flake.nix
+++ flake.nix
@@ -5,28 +5,50 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   };
 
-  outputs = { self, nixpkgs }: 
-      let
-        system = "x86_64-linux";
-	overlay = final: prev: {
-	  slock = prev.st.overrideAttrs (oldAttrs: rec {
-	    version = "main";
-	    src = builtins.path { path = ./.; name = "st"; };
-	    buildInputs = oldAttrs.buildInputs ++ [
-        prev.xorg.libXrandr.dev
-        prev.xorg.libXext.dev
-        prev.imlib2
-	    ];
-	  });
-	};
-	in
-	rec {
-	  overlays.default = overlay;
-	  checks.${system}.build = (
-            import nixpkgs {
-	      inherit system;
-	      overlays = [ overlay ];
-	    }
-	  ).slock;
-	};
+  outputs =
+    { self
+    , nixpkgs
+    ,
+    }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs { inherit system; };
+      overlay = final: prev: {
+        slock = prev.st.overrideAttrs (oldAttrs: rec {
+          version = "main";
+          src = builtins.path {
+            path = ./.;
+            name = "slock";
+          };
+          buildInputs =
+            oldAttrs.buildInputs
+            ++ [
+              prev.pkg-config
+              prev.xorg.libXrandr.dev
+              prev.xorg.libXext.dev
+              prev.imlib2
+            ];
+        });
+      };
+    in
+    rec {
+      devShells.x86_64-linux.default = pkgs.mkShell {
+        name = "slock-dev";
+        packages = with pkgs; [
+          gcc
+          pkg-config
+          xorg.libXrandr
+          xorg.libXext
+          imlib2
+        ];
+      };
+      overlays.default = overlay;
+      checks.${system}.build =
+        (
+          import nixpkgs {
+            inherit system;
+            overlays = [ overlay ];
+          }
+        ).slock;
+    };
 }
